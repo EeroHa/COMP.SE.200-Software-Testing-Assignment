b905e48d29007d8749ac24d7fe980128dce0250d
eero <hakaneneero@gmail.com>
2024-12-03 15:57:44 +0200

    Improve isDate coverage with mocked nodeTypes

---
+++ tests/isDateMock.test.js
@@ -0,0 +1,34 @@
+import isDate from '../src/isDate'
+import { describe, test, expect, jest } from '@jest/globals'
+
+jest.mock('../src/.internal/nodeTypes', () => {
+  /** Used to access faster Node.js helpers. */
+  return undefined
+})
+
+describe('isDate when !nodeIsDate', () => {
+  test('should return true for Date objects', () => {
+    expect(isDate(new Date())).toBe(true)
+    expect(isDate(new Date('2021-01-01'))).toBe(true)
+  })
+
+  test('should return false for non-Date objects', () => {
+    expect(isDate('Mon April 23 2012')).toBe(false)
+    expect(isDate(1617184800000)).toBe(false)
+    expect(isDate({})).toBe(false)
+    expect(isDate([])).toBe(false)
+    expect(isDate(null)).toBe(false)
+    expect(isDate(undefined)).toBe(false)
+    expect(isDate(true)).toBe(false)
+    expect(isDate(false)).toBe(false)
+    expect(isDate(/regex/)).toBe(false)
+  })
+
+  test('should return false for objects that are not Date instances but have similar properties', () => {
+    const fakeDate = {
+      toString: () => '[object Date]',
+      getTime: () => 1617184800000,
+    }
+    expect(isDate(fakeDate)).toBe(false)
+  })
+})
